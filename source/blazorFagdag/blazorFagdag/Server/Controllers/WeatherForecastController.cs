12554cf474aa38039f2352890a047dadba70cf1a
Stig Hausberg <Stig.Hausberg@norne.no>
2021-11-11 07:07:59 +0100

    ryddet opp

--- source/blazorFagdag/blazorFagdag/Server/Controllers/WeatherForecastController.cs
+++ source/blazorFagdag/blazorFagdag/Server/Controllers/WeatherForecastController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using blazorFagdag.Server.Dto;
 using blazorFagdag.Shared;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using RestSharp;
 
 namespace blazorFagdag.Server.Controllers
@@ -14,18 +13,6 @@ namespace blazorFagdag.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
@@ -37,15 +24,13 @@ namespace blazorFagdag.Server.Controllers
             YrData yr = response.Data;
             
             var weatherForecasts = yr.properties.timeseries.Select(timesery => new WeatherForecast
-                {
-                    Date = timesery.time,
-                    TemperatureC = timesery.data.instant.details.air_temperature,
-                    Summary = timesery.data.next_1_hours?.summary.symbol_code
-                })
-                .ToList();
+            {
+                Date = timesery.time,
+                TemperatureC = timesery.data.instant.details.air_temperature,
+                Summary = timesery.data.next_1_hours?.summary.symbol_code
+            }).ToList();
+            
             return weatherForecasts;
         }
-        
-        
     }
 }
