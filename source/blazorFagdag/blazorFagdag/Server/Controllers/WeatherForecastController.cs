46b8088863378c96f19a7c3ee534852cc93e0c06
Stig Hausberg <Stig.Hausberg@norne.no>
2021-11-19 10:25:19 +0100

    Fjernet kode for å se hvor mye arbeid det er

--- source/blazorFagdag/blazorFagdag/Server/Controllers/WeatherForecastController.cs
+++ source/blazorFagdag/blazorFagdag/Server/Controllers/WeatherForecastController.cs
@@ -13,26 +13,6 @@ namespace blazorFagdag.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get(double lat, double lon)
-        {
-            var latString = lat.ToString().Replace(',', '.');
-            var lonString = lon.ToString().Replace(',', '.');
-            var client = new RestClient($"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latString}&lon={lonString}");
-            var request = new RestRequest();
-            
-            var response = await client.ExecuteGetAsync<YrData>(request);
-
-            YrData yr = response.Data;
-            
-            var weatherForecasts = yr.properties.timeseries.Select(timesery => new WeatherForecast
-            {
-                Date = timesery.time,
-                TemperatureC = timesery.data.instant.details.air_temperature,
-                Summary = timesery.data.next_1_hours?.summary.symbol_code
-            }).ToList();
-            
-            return weatherForecasts;
-        }
+        
     }
 }
